fbc25194eeaeeeea8d946c13a48aeaee29acee52
Lollergalad <norinho89@gmail.com>
2022-10-27 21:28:17 +0200

    2/Some modifications, seperations

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
-import { useState } from "react";
-
 import Countdown from "./components/Countdown/Countdown";
 
-import snowman from "./assets/images/snowman.png";
-import wreath from "./assets/images/winter-wreath.png";
-
-import classes from "./App.module.scss";
+import classes from "./styles/App.module.scss";
 
 const date = "2022-11-30T23:00:00";
 
 const App = () => {
-  const [isCalendarVisible, setIsCalendarVisible] = useState(false);
-
   return (
     <div className={classes.container}>
-      <div className={classes.countdownContainer}>
-        <div className={classes.snowmanContainer}>
-          <img src={snowman} alt="Snowman" />
-        </div>
-        <Countdown date={date} />
-        <div className={classes.wreathContainer}>
-          <img src={wreath} alt="Winter wreath" />
-        </div>
-      </div>
+      <Countdown date={date} />
     </div>
   );
 };
--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import snowman from "../../assets/images/snowman.png";
+import wreath from "../../assets/images/winter-wreath.png";
 import christmasGift from "../../assets/images/christmas-gift.png";
 import christmasTree from "../../assets/images/christmas-tree.png";
 import snowGlobe from "../../assets/images/snow-globe.png";
@@ -27,37 +29,47 @@ const Countdown = ({ date }) => {
   const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
 
   return (
-    <div className={classes.countdown}>
-      <div className={classes.box}>
-        <div className={classes.timePart}>{days < 10 ? `0${days}` : days}</div>
-        <div className={classes.text}>days</div>
+    <div className={classes.countdownContainer}>
+      <div className={classes.snowmanContainer}>
+        <img src={snowman} alt="Snowman" />
       </div>
-      <div className={classes.divider}>
-        <img src={christmasGift} alt="Christmas gift" />
-      </div>
-      <div className={classes.box}>
-        <div className={classes.timePart}>
-          {hours < 10 ? `0${hours}` : hours}
+      <div className={classes.countdown}>
+        <div className={classes.box}>
+          <div className={classes.timePart}>
+            {days < 10 ? `0${days}` : days}
+          </div>
+          <div className={classes.text}>days</div>
         </div>
-        <div className={classes.text}>hours</div>
-      </div>
-      <div className={classes.divider}>
-        <img src={christmasTree} alt="Christmas tree" />
-      </div>
-      <div className={classes.box}>
-        <div className={classes.timePart}>
-          {minutes < 10 ? `0${minutes}` : minutes}
+        <div className={classes.divider}>
+          <img src={christmasGift} alt="Christmas gift" />
         </div>
-        <div className={classes.text}>minutes</div>
-      </div>
-      <div className={classes.divider}>
-        <img src={snowGlobe} alt="Snow globe" />
-      </div>
-      <div className={classes.box}>
-        <div className={classes.timePart}>
-          {seconds < 10 ? `0${seconds}` : seconds}
+        <div className={classes.box}>
+          <div className={classes.timePart}>
+            {hours < 10 ? `0${hours}` : hours}
+          </div>
+          <div className={classes.text}>hours</div>
         </div>
-        <div className={classes.text}>seconds</div>
+        <div className={classes.divider}>
+          <img src={christmasTree} alt="Christmas tree" />
+        </div>
+        <div className={classes.box}>
+          <div className={classes.timePart}>
+            {minutes < 10 ? `0${minutes}` : minutes}
+          </div>
+          <div className={classes.text}>minutes</div>
+        </div>
+        <div className={classes.divider}>
+          <img src={snowGlobe} alt="Snow globe" />
+        </div>
+        <div className={classes.box}>
+          <div className={classes.timePart}>
+            {seconds < 10 ? `0${seconds}` : seconds}
+          </div>
+          <div className={classes.text}>seconds</div>
+        </div>
+      </div>
+      <div className={classes.wreathContainer}>
+        <img src={wreath} alt="Winter wreath" />
       </div>
     </div>
   );
